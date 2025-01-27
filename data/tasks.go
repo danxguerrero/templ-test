db48d11374f3e83c1431433bf3b98565dcd35549
danxguerrero <danxguerrero@yahoo.com>
2025-01-26 21:21:18 -0500

    Define Data model for Task

--- data/tasks.go
+++ data/tasks.go
@@ -1,1 +1,32 @@
+package data
 
+type Task struct {
+	ID int
+	Title string
+	Completed bool
+}
+
+var Tasks = []Task{}
+var nextID = 1
+
+func AddTask(title string) {
+	Tasks = append(Tasks, Task{ID: nextID, Title: title, Completed: false})
+	nextID++
+}
+
+func ToggleTask(id int) {
+	for i, task := range Tasks {
+		if task.ID == id {
+			Tasks[i].Completed =  !Tasks[i].Completed
+			break
+		}
+	}
+}
+
+func DeleteTask(id int) {
+	for i, taks := range Tasks {
+		if taks.ID == id {
+			Tasks = append(Tasks[:i], Tasks[i+1:]...)
+		}
+	}
+}
