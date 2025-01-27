ada726ecc1b55238ae841f72763c47a089adb09e
danxguerrero <danxguerrero@yahoo.com>
2025-01-26 21:59:54 -0500

    Set up echo server in main.go

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"net/http"
 
+
+	"github.com/danxguerrero/templ-test/handlers"
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	e := echo.New()
 
+	e.Static("/static", "static")
+	e.GET("/", handlers.ShowIndex)
+	e.POST("/add", handlers.AddTask)
+	e.POST("/toggle", handlers.ToggleTask)
+	e.POST("/delete", handlers.DeleteTask)
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
